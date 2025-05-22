578634219f9c4a84ac68be9d169e6136acf738bd
Levi-Jordan <Levijordananderson@gmail.com>
2025-05-22 08:59:17 -0600

    Needs some finishing touches

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,11 @@
 export default function About() {
-    return <h1>About</h1>;
-}
+    return( 
+    <>
+    <div>
+        <h1>About</h1>
+        <p> This website was created using the NewsAPI. It takes in whatever query and returns recent articles that matches that criteria.
+            All credit goes to the creators of the API! </p>
+        <a href= "https://newsapi.org/">The News API</a>
+    </div>
+    </>
+)}
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,3 +1,63 @@
-export default function Homepage(){
-    return <h1> News </h1>;
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+//API KEY 
+
+export default function Homepage() {
+  // Our api key from some third-party API.
+  const apiKey = "2eb30dba45f2448597d2e61ad03b1942";
+  const url = `https://newsapi.org/v2/everything?q=gaming&apiKey=2eb30dba45f2448597d2e61ad03b1942`;
+
+  // State to hold the data.
+  const [data, setData] = useState(null);
+
+  // Function to fetch data.
+  const getData = async () => {
+    try {
+      const response = await axios.get(url);
+      setData(response.data);
+    } catch(e) {
+      console.error(e)
+    }
+  };
+
+  // useEffect to run getData when component mounts.
+  useEffect(() => {
+    getData();
+  }, []);
+
+  // loaded function for when data is fetched.
+const loaded = () => {
+    return (
+      <div>
+      <h1> News in Gaming</h1>
+      <ul>
+        {data.articles.map((article, index) => (
+          <li key={index}>
+            <a href={article.url} target="_blank">
+              <h3>{article.title}</h3>
+            </a>
+            <p>{article.description}</p>
+            {article.urlToImage && (
+              <img src={article.urlToImage} width="500" />
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+  };
+
+
+  // Function for when data doesn't exist.
+  const loading = () => {
+    return <h1>Loading...</h1>;
+  };
+
+  // If data exists, run the loaded function; otherwise, run loading.
+  return data ? loaded() : loading();
 }
+
+// //export default function Homepage(){
+//     return <h1> News </h1>;
+// }
